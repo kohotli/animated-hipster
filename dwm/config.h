c9caa33aaae200a5a76c9528468fc71283f7af25
Amelia Carter <ameliamarycarter@gmail.com>
2020-04-10 17:56:19 -0700

    Updated dwm config

--- dwm/config.h
+++ dwm/config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki Nerd Font Mono:pixelsize=24" "Blobmoji" };
-static const char dmenufont[]       = "mononoki Nerd Font Mono:pixelsize=24";
+static const char *fonts[]          = { "mononoki Nerd Font Mono:pixelsize=32" "Blobmoji" };
+static const char dmenufont[]       = "mononoki Nerd Font Mono:pixelsize=32";
 /* static const char col_border[]      = "#282A36";
 static const char col_sel_bg[]      = "#282A36";
 static const char col_sel_fg[]      = "#FF79C6";
